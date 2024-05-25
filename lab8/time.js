7fc4a3c1c796e9923e27b98d2304d25dd61da048
Киценко Анастасия <kicenkoanastasia625@gmail.com>
2024-05-25 16:08:08 +0700

    аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -3,5 +3,33 @@ function showTime(){
     let currentTime = today.toLocaleTimeString('ru-Ru');
     document.getElementById('time').innerHTML = currentTime;
 
-}
-setInterval(showTime,1000);
+    const hr = document.querySelector('.hr') 
+    const min = document.querySelector('.min') 
+    const sec = document.querySelector('.sec') 
+    
+
+    setInterval(() => { 
+    
+
+        let day = new Date() 
+        let hour = day.getHours() 
+        let minutes = day.getMinutes() 
+        let seconds = day.getSeconds() 
+    
+        
+        let hrrotation = (30 * hour) + (0.5 * minutes); 
+        let minrotation = 6 * minutes; 
+        let secrotation = 6 * seconds; 
+    
+        hr.style.transform = 
+            `translate(-50%,-100%) rotate(${hrrotation}deg)` 
+        min.style.transform = 
+            `translate(-50%,-100%) rotate(${minrotation}deg)` 
+        sec.style.transform = 
+            `translate(-50%,-85%) rotate(${secrotation}deg)` 
+    });
+    }
+
+showTime(); // Вызываем функцию при загрузке страницы
+
+setInterval(showTime, 1000);
